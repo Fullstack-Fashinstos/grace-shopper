41a6c64ed8a91b65d15cc9112e87be0105ddcc33
Will <willchristensen27@gmail.com>
2021-06-17 11:00:47 -0400

    added admin

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -6,10 +6,66 @@ import { fetchSingleProduct } from '../store/singleProduct'
 class SingleProduct extends React.Component {
     constructor() {
         super()
+        this.state = {
+            name: '',
+            imageUrl: '',
+            descrpition: '',
+            price: '',
+            stock: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount() {
         this.props.fetchSingleProduct(this.props.match.params.productId)
+        //const {description, imageUrl, name, price, stock} = this.props.singleProduct
+        //console.log(description)
+        // this.setState({
+        //     name: name,
+        //     imageUrl: imageUrl,
+        //     description: description,
+        //     price: price,
+        //     stock: stock
+
+        // })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
+    handleChange(event) {
+        
+        const type = event.target.name
+        const val = event.target.value
+        console.log(val)
+        if(type === 'name') {
+            this.setState({
+                name: val
+            })
+        }
+        if(type === 'price') {
+            this.setState({
+                price: val
+            })
+        }
+        if(type === 'description') {
+            this.setState({
+                description: val
+            })
+        }
+        if(type === 'stock') {
+            this.setState({
+                stock: val
+            })
+        }
+        if(type === 'imageUrl') {
+            this.setState({
+                imageUrl: val
+            })
+        }
     }
 
     render() {
@@ -21,6 +77,58 @@ class SingleProduct extends React.Component {
                 <p>{description}</p>
                 <p>{price}</p>
                 <p>{stock}</p>
+
+                <h4>ADMIN</h4>
+                <form id="new-message-form" onSubmit={this.handleSubmit}>
+          <div className="input-group input-group-lg">
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="name"
+              value={this.state.name}
+              placeholder={name}
+            />
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="description"
+              value={this.state.description}
+              placeholder={description}
+            />
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="price"
+              value={this.state.price}
+              placeholder={price}
+            />
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="imageUrl"
+              value={this.state.imageUrl}
+              placeholder={imageUrl}
+            />
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="stock"
+              value={this.state.stock}
+              placeholder={stock}
+            />
+            <span className="input-group-btn">
+              <button className="btn btn-default" type="submit">
+                Submit
+              </button>
+            </span>
+          </div>
+        </form>
+
             </div>
         )
     }
--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const SET_SINGLE_PRODUCT = 'SET_SINGLE_PRODUCT'
+const EDIT_PRODUCT = 'EDIT_PRODUCT'
 
 const setSingleProduct = (product) => {
     return {
@@ -9,17 +10,35 @@ const setSingleProduct = (product) => {
     }
 }
 
+const editProduct = (product) => {
+    return {
+        type: EDIT_PRODUCT,
+        product
+    }
+}
+
 export const fetchSingleProduct = (id) => {
     return async (dispatch) => {
         const { data } = await axios.get(`/api/products/${id}`)
-        console.log(data, 'in thunk')
         dispatch(setSingleProduct(data))
+    }
+}
+
+export const sendEditProduct = (product) => {
+    console.log(product, 'arg')
+    return async (dispatch) => {
+        const { data } = await axios.put(`/api/products${product.id}`)
+        console.log(data, 'in thunk')
+        dispatch(editProduct(data))
     }
 }
 
 export default function singleProduct (state = {}, action) {
     switch(action.type) {
         case SET_SINGLE_PRODUCT: {
+            return action.product
+        }
+        case EDIT_PRODUCT: {
             return action.product
         }
         default: return state
