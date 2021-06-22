3e7b7fbe8121fbaa7d99776876a26af685d23275
Andrew Larsen <ajlarsen2@gmail.com>
2021-06-22 11:21:37 -0400

    Feature - Checkout and update stock

--- client/components/CheckoutBox.js
+++ client/components/CheckoutBox.js
@@ -2,37 +2,13 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { checkoutCartThunk } from "../store/cart";
-import axios from 'axios';
 
 
 export class CheckoutBox extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      warning: ''
-    }
     this.handleCheckout = this.handleCheckout.bind(this);
-    // this.verifyStock = this.verifyStock.bind(this);
   }
-  // async verifyStock() {
-  //   let cartProducts = [];
-  //   if (this.props.userId) {
-  //     cartProducts = this.props.memberCart.order_products;
-  //   } else {
-  //     cartProducts = this.props.visitorCart.order_products;
-  //   }
-  //   for (let i = 0; i < cartProducts.length; i++) {
-  //     console.log('cart products', cartProducts[i]);
-  //     let { data : currentItem } = await axios.get(`/api/products/${cartProducts[i].product.id}`);
-  //     console.log('check stock')
-  //     console.log(currentItem.stock)
-  //     console.log(cartProducts[i].quantity)
-  //     if (currentItem.stock < cartProducts[i].quantity) {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // }
   handleCheckout() {
     if (this.props.userId) {
       this.props.checkoutCart(this.props.memberCart.order_products, this.props.userId);
