d6ad9821f3f349aa4891f71cb25053dd780fbec8
shapirodaniel <shapirodanieladam@gmail.com>
2021-06-23 17:42:18 -0500

    update: app, utils

--- client/App.js
+++ client/App.js
@@ -1,39 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import Navbar from "./components/Navbar";
-import Routes from "./Routes";
-import {connect} from 'react-redux'
-import { fetchUsers } from '../client/store/auth'
+import Navbar from './components/Navbar';
+import Routes from './Routes';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../client/store/auth';
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.attemptLogin = this.attemptLogin.bind(this)
-    //this.fetchUsers = this.fetchUsers.bind(this)
-  }
+	constructor() {
+		super();
+	}
 
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<Routes />
+			</div>
+		);
+	}
+}
 
-  componentDidMount() {
-    this.attemptLogin()
-  }
-  async attemptLogin() {
-    const token = window.localStorage.getItem('token');
-    this.props.fetchUsers(token)
-  }
+const mapDispatch = dispatch => ({
+	fetchUsers: token => dispatch(fetchUsers(token)),
+});
 
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Routes />
-      </div>
-    );
-  }
-  
-};
-
-const mapDispatch = (dispatch) => ({
-  fetchUsers: (token) => dispatch(fetchUsers(token))
-})
-
-export default connect(null, mapDispatch)(App)
+export default connect(null, mapDispatch)(App);
--- server/api/utils.js
+++ server/api/utils.js
@@ -1,65 +1,23 @@
-const User = require('../db/models/user')
-
-
-
+const User = require('../db/models/user');
 
 const isAdmin = async (req, res, next) => {
-    //console.log('hit')
-    //console.log(req.headers, 'headers')
-    try {
-        const token = req.headers.authorization;
-        console.log(token)
-        const user = await User.findByToken(token);
-        console.log(user, 'is admin')
-        if(user.isAdmin) {
-            //console.log('hit')
-            next()
-        } else {
-            //console.log('in else')
-            const error = new Error('unauthurized')
-            error.status = 401
-            next(error)
-        }
-        
-      } catch(error) {
-          //console.log('catch')
-        next(error);
-      }
-}
+	try {
+		const token = req.headers.authorization;
 
-// const userAuth = async (req, res, next) => {
-//     next()
-// }
+		const user = await User.findByToken(token);
 
-// const adminHeaderAuth = async (req, res, next) => {
-//     if(req.headers.admin === 'true') {
-//         next()
-//     } else {
-//         const error = new Error('unauth')
-//         error.status = 403
-//         throw error
-//     }
-// }
+		if (user.isAdmin) {
+			return next();
+		}
 
+		const error = new Error(
+			'only admins or authorized users can access this route'
+		);
+		error.status = 401;
+		next(error);
+	} catch (err) {
+		next(err);
+	}
+};
 
-// const adminAuth = async (req, res, next) => {
-//     try {
-//         console.log(req.headers, 'in adminAuth')
-//         if(req.body.isAdmin) {
-//             next()
-//         } 
-//         if(req.body.user.isAdmin) {
-//             next()
-//         } else {
-//             const error = new Error('unauth')
-//             error.status = 403
-//             throw error
-           
-//         }
-
-//     } catch (error) {
-//         next(error)
-//     }
-// }
-
-module.exports = isAdmin
+module.exports = isAdmin;
